bf3a1aa38a53d24f00551968a521c4804784ae7e
Kevin Burke <kevin@twilio.com>
2013-04-21 10:09:21 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FILEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $FILEDIR
+
+PATHDIRS="
+/usr/local
+/opt/local
+$HOME/bin
+/usr/bin"
+
+for dir in $PATHDIRS
+do
+    if [ `echo $PATH | grep $dir` ]; then
+        cp -rvf "$FILEDIR/bin" "$FILEDIR/games" $dir
+        break
+    fi
+done
