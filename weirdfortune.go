0e8bc62375f695d5aa14f2c9db845d23dafe6ece
Kevin Burke <kev@inburke.com>
2014-11-30 17:19:55 -0800

    implement the program

--- weirdfortune.go
+++ weirdfortune.go
@@ -3,26 +3,49 @@ package main
 import (
 	"bytes"
 	"flag"
-	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func main() {
-	_ = flag.Bool("all", false, "Display all fortunes including NSFW ones")
+	all := flag.Bool("all", false, "Display all fortunes including NSFW ones")
 	directory := flag.String("directory", "/usr/local/games", "Path to the games folder")
 	flag.Parse()
 	if len(flag.Args()) > 0 {
-		log.Fatalf("Usage: weirdfortune -all")
+		log.Fatalf("Usage: weirdfortune [-all] [-directory DIRECTORY] ")
 	}
 
 	f, err := os.Open(filepath.Join(*directory, "weirdfortunes", "weirdfortunes"))
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	fmt.Println(f.Name())
 	b := new(bytes.Buffer)
 	b.ReadFrom(f)
-	fmt.Println(b.String())
+
+	if *all {
+		b.WriteString("%\n")
+		f, err = os.Open(filepath.Join(*directory, "weirdfortunes", "nsfwweirdfortunes"))
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		b.ReadFrom(f)
+	}
+	// XXX: remove duplicates
+	tweets := strings.Split(b.String(), "\n%\n")
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	choice := r.Intn(len(tweets))
+	usertweet := strings.SplitN(tweets[choice], ": ", 2)
+
+	os.Stdout.WriteString("\033[0;31m")
+	os.Stdout.WriteString(usertweet[0])
+	os.Stdout.WriteString("\033[0m")
+	os.Stdout.WriteString("\n")
+	os.Stdout.WriteString("\033[0;34m")
+	os.Stdout.WriteString(usertweet[1])
+	os.Stdout.WriteString("\033[0m")
+	os.Stdout.WriteString("\n")
 }
